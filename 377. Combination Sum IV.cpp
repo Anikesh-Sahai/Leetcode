a5aa262ed8380fb96a246cb63f42d750bd48747c
Anikesh-Sahai <109746384+Anikesh-Sahai@users.noreply.github.com>
2023-02-06 00:09:53 +0530

    Add files via upload

---
+++ 377. Combination Sum IV.cpp
@@ -0,0 +1,24 @@
+//377. Combination Sum IV
+
+
+class Solution {
+public:
+    int help(vector<int>& nums, int target,vector<int>& dp){
+        if(target==0) return 1;
+        if(target<0) return 0;
+
+        if(dp[target] != -1) return dp[target];
+        int ans=0;
+
+        for(int i=0;i<nums.size();i++){
+            ans=ans+help(nums,target-nums[i],dp);
+        }
+        return dp[target]=ans;
+    }
+
+
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1,-1);
+        return help(nums,target,dp);
+    }
+};
